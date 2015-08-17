3ceb42b6e895da7b3cf765733cab99eb53e52281
RahulZoldyck <rahulzoldyck@gmail.com>
2015-08-18 01:37:58 +0530

    Weekly TT updated

---
+++ app/src/main/java/com/delta/attendancemanager/APIhandler.java
@@ -0,0 +1,15 @@
+package com.delta.attendancemanager;
+
+
+import android.os.AsyncTask;
+
+public class APIhandler {
+    class getTT extends AsyncTask<Void,Void,Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+
+
+            return null;
+        }
+    }
+}
--- app/src/main/java/com/delta/attendancemanager/CRSlidingTabsFragment.java
+++ app/src/main/java/com/delta/attendancemanager/CRSlidingTabsFragment.java
@@ -11,12 +11,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-/**
- * Created by S on 1/20/2015.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HandshakeCompletedListener;
+
 public class CRSlidingTabsFragment extends Fragment{
 
-
+    MySqlHandler handler;
+    List<String[]> all;
     private SlidingTabLayout mSlidingTabLayout;
 
     private ViewPager mViewPager;
@@ -93,6 +96,29 @@ public class CRSlidingTabsFragment extends Fragment{
          */
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
+            handler=new MySqlHandler(getActivity(),null);
+            all=new ArrayList<>();
+            all=handler.get_days();
+            String[] m,t,w,th,f,x;
+            m=new String[9];
+            t=new String[9];
+            w=new String[9];
+            th=new String[9];
+            f=new String[9];
+            x=new String[9];
+            for (String[] i : all){
+                if(i[0].equals("Monday"))
+                    m=i;
+                else if(i[0].equals("Tuesday"))
+                    t=i;
+                else if(i[0].equals("Wednesday"))
+                    w=i;
+                else if(i[0].equals("Thursday"))
+                    th=i;
+                else if(i[0].equals("Friday"))
+                    f=i;
+            }
+
             // Inflate a new layout from our resources
             View view = getActivity().getLayoutInflater().inflate(R.layout.pager_item,
                     container, false);
@@ -107,6 +133,33 @@ public class CRSlidingTabsFragment extends Fragment{
             TextView sub6 = (TextView) view.findViewById(R.id.sub6);
             TextView sub7 = (TextView) view.findViewById(R.id.sub7);
             TextView sub8 = (TextView) view.findViewById(R.id.sub8);
+
+            switch (position){
+                case 0:
+                    x=m;
+                    break;
+                case 1:
+                    x=t;
+                    break;
+                case 2:
+                    x=w;
+                    break;
+                case 3:
+                    x=th;
+                    break;
+                case 4:
+                    x=f;
+                    break;
+            }
+            sub1.setText(x[1]);
+            sub2.setText(x[2]);
+            sub3.setText(x[3]);
+            sub4.setText(x[4]);
+            sub5.setText(x[5]);
+            sub6.setText(x[6]);
+            sub7.setText(x[7]);
+            sub8.setText(x[8]);
+
 
             sub1.setOnClickListener(new View.OnClickListener() {
                 @Override
