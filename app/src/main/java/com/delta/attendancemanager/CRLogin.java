bbe4d9b3f12f05ddbeda47dfd17cc0163b898d2c
Rahul Zoldyck <rahulzoldyck@gmail.com>
2016-01-12 14:33:53 +0530

    CR Login Finished

--- app/src/main/java/com/delta/attendancemanager/CRLogin.java
+++ app/src/main/java/com/delta/attendancemanager/CRLogin.java
@@ -3,17 +3,22 @@ package com.delta.attendancemanager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 
 public class CRLogin extends ActionBarActivity {
-
+    EditText username,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,9 +28,40 @@ public class CRLogin extends ActionBarActivity {
         crlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                EditText username = (EditText) findViewById(R.id.crbranch);
-                EditText password = (EditText) findViewById(R.id.crpass);
-                String rno = username.getText().toString();
+                 username = (EditText) findViewById(R.id.crbranch);
+                 password = (EditText) findViewById(R.id.crpass);
+                CrAuth auth = new CrAuth();
+                auth.execute(username.getText().toString(),password.getText().toString());
+
+            }
+        });
+    }
+
+    class CrAuth extends AsyncTask<String,Void,Boolean>{
+        JSONParser jp=new JSONParser();
+         String usernameString;
+        @Override
+        protected Boolean doInBackground(String... params) {
+            try {
+                JSONObject js=new JSONObject();
+                usernameString = params[0];
+                js.put("username",params[0]);
+                js.put("password", params[1]);
+                JSONObject jd=jp.makeHttpRequest(MainActivity.URL+"/crlogin","POST",js);
+                Log.i("ls", js.toString());
+                int success=jd.getInt("Signed Up");
+                return success==1;                                                //authentication
+            }  catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            return false;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if(aBoolean){
+                String rno = usernameString;
                 SharedPreferences share=getSharedPreferences("user", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor=share.edit();
                 editor.putString("crrno", rno);
@@ -33,9 +69,14 @@ public class CRLogin extends ActionBarActivity {
                 Intent i = new Intent(CRLogin.this, CRhome.class);
                 startActivity(i);
             }
-        });
+            else {
+                username.setText("");
+                password.setText("");
+                TextView err = (TextView) findViewById(R.id.wrongPassword);
+                err.setText("Wrong Roll No or Password");
+            }
+        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
